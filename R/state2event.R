7203f0d8378c08b9e1da6640b90b79b280bff166
Henrik Renlund <henrikrenlund@gmail.com>
2024-02-19 15:51:36 +0100

    warning added to state2event as this is clearly not finished

--- R/state2event.R
+++ R/state2event.R
@@ -13,6 +13,7 @@
 ##' cbind(d, state2event(d$state))
 ##' @export
 state2event <- function(s){
+    warning("This function is not ready for use, does not take an 'id' into account")
     properties(s, class = c("character", "factor"), na.ok = FALSE)
     if(!is.factor(s)) s <- factor(s)
     lev <- levels(s)
